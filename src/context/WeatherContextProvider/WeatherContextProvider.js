4dc4549113b6031511cadc323107b7179682b117
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-01-08 21:19:30 +0100

    geoLocation effect added

--- src/context/WeatherContextProvider/WeatherContextProvider.js
+++ src/context/WeatherContextProvider/WeatherContextProvider.js
@@ -1,12 +1,38 @@
-import { createContext, useMemo } from 'react';
+import { createContext, useEffect, useMemo, useState } from 'react';
 
 const WeatherContext = createContext(null);
 
 const WeatherContextProvider = ({ children }) => {
-  const contextValue = useMemo(() => {});
+  const [location, setLocation] = useState({
+    latitude: undefined,
+    longitude: undefined,
+  });
+  const [islocationErrorPresent, setIslocationErrorPresent] = useState(false);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocation({ latitude, longitude });
+        },
+        () => setIslocationErrorPresent(true),
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+      setIslocationErrorPresent(true);
+    }
+  }, []);
+
+  const contextValue = useMemo(() => {
+    return {
+      geoLocation: location,
+      islocationErrorPresent: islocationErrorPresent,
+    };
+  }, []);
 
   return (
-    <WeatherContext.Provider value={{ contextValue }}>
+    <WeatherContext.Provider value={contextValue}>
       {children}
     </WeatherContext.Provider>
   );
