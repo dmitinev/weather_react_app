144a44812d64df58413d18488ef431839b3a6c13
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-01-09 16:55:39 +0100

    added location defining, and weather check effect

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,19 @@
+import { useContext, useEffect, useState } from 'react';
+import { WeatherContext } from '../../context/WeatherContextProvider/WeatherContextProvider';
 import SvgIcon from '../SvgIcons';
 import style from './SearchBar.module.scss';
 
 const SearchBar = () => {
+  const { locationCity } = useContext(WeatherContext);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    setValue(locationCity);
+  }, [locationCity]);
+  const changeHandler = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <form className={style.searchBar} data-testid="searchBar">
       <input
@@ -10,6 +22,8 @@ const SearchBar = () => {
         className={style.searchBar__search}
         autoComplete="off"
         minLength={3}
+        value={value}
+        onChange={changeHandler}
       />
       <SvgIcon type="Search" width="1.75rem" />
     </form>
--- src/context/WeatherContextProvider/WeatherContextProvider.js
+++ src/context/WeatherContextProvider/WeatherContextProvider.js
@@ -1,35 +1,54 @@
+import axios from 'axios';
 import { createContext, useEffect, useMemo, useState } from 'react';
 
-const WeatherContext = createContext(null);
+const BASE_URL_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHERAPI_KEY}`;
+
+export const WeatherContext = createContext(null);
 
 const WeatherContextProvider = ({ children }) => {
   const [location, setLocation] = useState({
     latitude: undefined,
     longitude: undefined,
   });
-  const [islocationErrorPresent, setIslocationErrorPresent] = useState(false);
+  const [city, setCity] = useState('London');
+  const [currentWeather, setCurrentWeather] = useState({});
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });
-        },
-        () => setIslocationErrorPresent(true),
-      );
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ latitude, longitude });
+      });
     } else {
       console.error('Geolocation is not supported by this browser.');
-      setIslocationErrorPresent(true);
     }
   }, []);
+
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      setCity('');
+    }
+  }, [location]);
+
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      axios(
+        BASE_URL_WEATHER + `&q=${location.latitude},${location.longitude}`,
+      ).then(({ data }) => {
+        setCity(data.location.name);
+        setCurrentWeather(data.current);
+        console.log(data);
+      });
+    }
+  }, [location]);
 
   const contextValue = useMemo(() => {
     return {
       geoLocation: location,
-      islocationErrorPresent: islocationErrorPresent,
+      locationCity: city,
+      currentWeather: currentWeather,
     };
-  }, []);
+  }, [location, city, currentWeather]);
 
   return (
     <WeatherContext.Provider value={contextValue}>
