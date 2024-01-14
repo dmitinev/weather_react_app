d24eebada53cd6c3c5e1b4c4aa5a2f3c66772a4f
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-01-14 12:17:47 +0100

    added loading for aside section forecast

--- src/components/AsideWeatherForecast/AsideWeatherForecast.js
+++ src/components/AsideWeatherForecast/AsideWeatherForecast.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
+import { WeatherContext } from '../../context/WeatherContextProvider/WeatherContextProvider';
+import WeatherDataSkeleton from '../../loadingSkeletons/WeatherDataSkeleton';
 import WeatherForecastData from '../WeatherForecastData';
 import style from './AsideWeatherForecast.module.scss';
 
 const AsideWeatherForecast = () => {
+  const { isGeoLocationReceiving, dataIsLoading } = useContext(WeatherContext);
   return (
     <div
       className={style.asideWeatherForecast}
       data-testid="asideWeatherForecast"
     >
       <h2 className={style.asideWeatherForecast__heading}>Weather Forecast</h2>
-      <WeatherForecastData />
+      {isGeoLocationReceiving || dataIsLoading ? (
+        <WeatherDataSkeleton />
+      ) : (
+        <WeatherForecastData />
+      )}
     </div>
   );
 };
